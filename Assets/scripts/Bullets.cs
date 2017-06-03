9faf1cc1ce209ae726308a20bfc1166be1cdf6c1
lee1026td <lee1026td@gmail.com>
2017-06-03 16:34:52 +0900

    project
    플레이어 움직임
    플레이어 총쏘기
    충돌처리

---
+++ Assets/scripts/Bullets.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+	Enemy e;
+	public LayerMask collisionMask;
+	public float spd = 10; // 총탄이 나가는 속도 설정
+	public void SetSpd(float newSpd) {
+		spd = newSpd;
+	}
+	void Update () {
+		float moveDistance = spd * Time.deltaTime; 
+		transform.Translate (Vector3.up * moveDistance);
+	} //총알 발사
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log("OnTriggerEnter");
+		if(col.gameObject.name == "Enemy") {
+			Destroy(col.gameObject);
+			Debug.Log("Destroy");
+		}
+	}
+}
