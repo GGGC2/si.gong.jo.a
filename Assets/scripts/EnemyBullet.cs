59127fc9cd38f544e9b7b36401df7a4256ac81e9
박종훈 <qkrwhdgnsel@naver.com>
2017-06-03 16:00:32 +0900

    enemybullet, enemy사진, enemybullet사진,enemyshooter

---
+++ Assets/scripts/EnemyBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		float delta = 1.0f * Time.deltaTime;
+
+		transform.position = new Vector3(transform.position.x + delta, transform.position.y, transform.position.z);
+	}
+}
