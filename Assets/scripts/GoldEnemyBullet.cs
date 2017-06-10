bd8b19527f7fc420dae2b6b52a1acb84268fa8ea
박종훈 <qkrwhdgnsel@naver.com>
2017-06-10 15:07:13 +0900

    적

---
+++ Assets/scripts/GoldEnemyBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldEnemyBullet : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		float delta = 5.0f * Time.deltaTime;
+
+		Vector3 deltaVec = transform.right * delta;
+		transform.position = transform.position + deltaVec;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name.Contains("player")) {
+			Destroy(gameObject);
+			other.SendMessage("hit");
+		}
+		if(other.gameObject.name.Contains("wall")){
+			Destroy(gameObject);
+		}
+	}
+}
