b1cdee5e422527aac63c860ddeff3667f7b54d47
Juhyeong Park <majecty@gmail.com>
2017-06-13 13:02:19 +0900

    Make enemy find nearest enemy

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,62 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
     public Transform target;
-    private GameObject player;
-    private GameObject player2;
     private GameObject enemy;
-    private float range;
     public float Bronzespd;
 
-    void Start() {
-      //  enemy = GameObject.FindGameObjectWithTag ("Enemy");
-      
-        player = GameObject.FindGameObjectWithTag ("Player");
-        player2 = GameObject.FindGameObjectWithTag("Player2");
+    void Start()
+    {
     }
 
-    void Update() {
-        if(player != null) {
-           
+    void Update()
+    {
+        float range = 5.0f;
+        var nearPlayer = PlayerFinder.Instance.FindNearestPlayer(transform.position, range);
+
+        if (nearPlayer == null)
+        {
+
         }
-
-        GameObject nearPlayer;
-
-        if (player == null && player2 == null) {
-
+        else
+        {
+            FollowPlayer(nearPlayer);
         }
-        else if (player== null) {
-            FollowPlayer(player2);
-        }
-        else if (player2 == null) {
-            FollowPlayer(player);
-        }
-        else {
-            var range1 = Vector2.Distance (transform.position, player.transform.position);
-        
-            var range2 = Vector2.Distance (transform.position, player2.transform.position);
-
-            if (range1 <= range2) {
-FollowPlayer(player);
-            } else {
-FollowPlayer(player2);
-            }
-        }
-
-
     }
 
-    void FollowPlayer(GameObject player) {
-         range = Vector2.Distance (transform.position, player.transform.position);
-               if(range <= 5f) {
-                    Debug.Log(transform.position);
-                    Debug.Log(player.transform.position);
-                    Debug.Log(Vector2.MoveTowards (transform.position, player.transform.position, range) );
-
-                    Debug.Log(Vector2.MoveTowards (transform.position, player.transform.position, range) * Bronzespd * Time.deltaTime);
-                  transform.position = Vector3.MoveTowards (transform.position, player.transform.position, range * Bronzespd * Time.deltaTime) ;
-               }
+    void FollowPlayer(GameObject player)
+    {
+        float range = Vector2.Distance(transform.position, player.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, range * Bronzespd * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D col)
---
+++ Assets/scripts/PlayerFinder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFinder : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject player2;
+
+	public static PlayerFinder Instance;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public GameObject FindNearestPlayer(Vector3 position, float range) {
+		GameObject nearest = FindNearestPlayer(position);
+        var nearestRange = Vector2.Distance(position, nearest.transform.position);
+		if (nearestRange > range) {
+			return null;
+		}
+		return nearest;
+	}
+
+	public GameObject FindNearestPlayer(Vector3 position) {
+		if (player == null && player2 == null) {
+			return null;
+		}
+
+		if (player == null) {
+			return player2;
+		}
+
+		if (player2 == null) {
+			return player;
+		}
+
+        var range1 = Vector2.Distance(position, player.transform.position);
+        var range2 = Vector2.Distance(position, player2.transform.position);
+
+		if (range1 < range2) {
+			return player;
+		} else {
+			return player2;
+		}
+	}
+}
