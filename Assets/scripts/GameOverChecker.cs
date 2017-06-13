8b5d4d99588ac051e4bfc54deab2c47a9051c843
Juhyeong Park <majecty@gmail.com>
2017-06-13 13:21:38 +0900

    Add game over checker

---
+++ Assets/scripts/GameOverChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverChecker : MonoBehaviour
+{
+
+    public GameObject gameOverImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerFinder.Instance.player != null)
+        {
+            return;
+        }
+
+        if (PlayerFinder.Instance.player2 != null)
+        {
+            return;
+        }
+
+        Debug.Log("Player all dead");
+        gameOverImage.SetActive(true);
+    }
+}
